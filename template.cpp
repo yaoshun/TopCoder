4999762cdc53e32271c0ad17b53ded51a271d682
Shun Yao <yaoshun@Shuns-MacBook-Pro.local>
2016-03-26 11:57:49 -0400

    added template for topcoder competition

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class ClassName {
+public:
+    int functionName(vector<int> table) {
+    }
+};
